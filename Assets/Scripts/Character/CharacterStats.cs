e2fed3b79bdf6617a5892d07e88d8070891a8c58
Marc Zaku <mzaku@hotmail.de>
2024-03-13 14:46:24 +0100

    Add First Character Stat Prototype

---
+++ Assets/Scripts/Character/CharacterStats.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CharacterStats : MonoBehaviour
+{
+    public UnityEvent<int> IntelligenceChange;
+    [SerializeField] private int _intelligence;
+    public int GetIntelligence()
+    {
+        return _intelligence;
+    }
+    public void SetIntelligence(int value)
+    {
+        _intelligence = value;
+        IntelligenceChange.Invoke(value);
+    }
+
+    public UnityEvent<int> StrengthChange;
+    [SerializeField] private int _strength;
+    public int Strength
+    {
+        get => _strength;
+        set
+        {
+            _strength = value;
+            StrengthChange.Invoke(value);
+        }
+    }
+
+    public void LevelUp()
+    {
+        Strength += 5;
+        SetIntelligence(GetIntelligence() + 3);
+    }
+}
---
+++ Assets/Scripts/UI/CharacterStatsUI.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CharacterStatsUI : MonoBehaviour
+{
+    public TMP_Text IntelligenceLabel;
+    public TMP_Text StrengthLabel;
+    public CharacterStats Stats;
+    
+    void Start()
+    {
+        Stats.IntelligenceChange.AddListener(OnIntelligenceChange);
+        Stats.StrengthChange.AddListener(OnStrengthChange);
+    }
+
+    private void OnStrengthChange(int newStrength)
+    {
+        StrengthLabel.text = newStrength.ToString();
+    }
+
+    private void OnIntelligenceChange(int newIntelligence)
+    {
+        IntelligenceLabel.text = newIntelligence.ToString();
+    }
+}
